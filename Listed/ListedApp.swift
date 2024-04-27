2aa227c49d1d895a4ab13431752a40bc669abf25
mandeep2008 <mandeepwbk20@gmail.com>
2024-04-27 23:21:35 +0530

    Initial Commit

---
+++ Listed/ListedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListedApp.swift
+//  Listed
+//
+//  Created by Prabh on 27/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ListedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
